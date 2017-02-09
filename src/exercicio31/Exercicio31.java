b2979f9f7ca171434d4a40b3d57d9e6298720359
oracle <oracle@localhost>
2017-02-09 10:27:06 +0100

    Finalizado

--- src/exercicio31/Exercicio31.java
+++ src/exercicio31/Exercicio31.java
@@ -2,25 +2,27 @@ package exercicio31;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 import org.neodatis.odb.ODB;
 import org.neodatis.odb.ODBFactory;
 import org.neodatis.odb.Objects;
 import org.neodatis.odb.core.query.IQuery;
+import org.neodatis.odb.core.query.criteria.ICriterion;
 import org.neodatis.odb.core.query.criteria.Where;
+import org.neodatis.odb.core.query.nq.SimpleNativeQuery;
 import org.neodatis.odb.impl.core.query.criteria.CriteriaQuery;
+import static sun.audio.AudioPlayer.player;
 
 public class Exercicio31 {
 
-    public static final String ODB_NAME ="BaseDatosax";
+    public static final String ODB_NAME = "BaseDatosaux";
+    static ODB odb;
+    static String depName = null;
 
     public static void main(String[] args) throws Exception {
-        
-        consulta_xogadores();
-        
-        
-        
 
+        
     }
 
     public static void step1() throws Exception {
@@ -75,10 +77,10 @@ public class Exercicio31 {
         Game game1 = new Game(new Date(), volleyball, team1, team2);
         Game game2 = new Game(new Date(), tenis, team3, team4);
         ODB odb = null;
-        
+
         try {
             odb = ODBFactory.open(ODB_NAME);
-            
+
             odb.store(game1);
             odb.store(game2);
         } finally {
@@ -87,30 +89,10 @@ public class Exercicio31 {
             }
         }
     }
-    
-    public static void step3() throws Exception {
-        ODB odb = null;
-        try {
-
-            odb = ODBFactory.open(ODB_NAME);
-            IQuery query = odb.criteriaQuery(Player.class, Where.equal("name", "pierre"));
-            Objects<Player> players = odb.getObjects(query);
-            System.out.println("\nStep 3 : Players with name olivier");
-            while (players.hasNext()) {
-                int i = 0;
-                System.out.println((i + 1) + "\t: " + players.next());
-            }
-        } finally {
-            if (odb != null) {
-
-                odb.close();
-            }
-        }
-    }
 
     public static void displaySports() throws Exception {
 
-        ODB odb = ODBFactory.open(ODB_NAME);
+        odb = ODBFactory.open(ODB_NAME);
         Objects<Sport> sports = odb.getObjects(Sport.class);
 
         Sport sport = null;
@@ -122,18 +104,153 @@ public class Exercicio31 {
         odb.close();
 
     }
-    public static void consulta_xogadores() throws Exception{
-        ODB odb = ODBFactory.open(ODB_NAME);
+
+    public static void consulta_xogadores() throws Exception {
+        odb = ODBFactory.open(ODB_NAME);
         Objects<Player> players = odb.getObjects(Player.class);
 
         Player player = null;
         while (players.hasNext()) {
             player = players.next();
-            System.out.println(player.getName()+","+player.getFavoriteSport().getName()+","+player.getSalario());
+            System.out.println(player.getName() + "," + player.getFavoriteSport().getName() + "," + player.getSalario());
         }
 
         odb.close();
 
     }
 
-}
+    public static void actualiza_por_nome_xogador(ODB odb, String nome_antigo, String nome_novo) {
+        odb = ODBFactory.open(ODB_NAME);
+        IQuery query = odb.criteriaQuery(Player.class, Where.equal("name", nome_antigo));
+        Objects<Player> players = odb.getObjects(query);
+        while (players.hasNext()) {
+            Player player = (Player) players.next();
+            player.setName(nome_novo);
+            odb.store(player);
+
+        }
+        odb.close();
+    }
+
+    public static void xogadoresDeporte(ODB odb, String deporte) {
+        odb = ODBFactory.open(ODB_NAME);
+        IQuery query_aux = odb.criteriaQuery(Sport.class, Where.equal("name", deporte));
+        Objects<Sport> sports = odb.getObjects(query_aux);
+        Sport sport = null;
+        while (sports.hasNext()) {
+            sport = sports.next();
+            System.out.println(sport.getName());
+        }
+        IQuery query = odb.criteriaQuery(Player.class, Where.equal("favoriteSport", sport));
+        Objects<Player> players = odb.getObjects(query);
+        players.size();
+        Player player = null;
+        while (players.hasNext()) {
+            player = players.next();
+            System.out.println(player.getName());
+
+        }
+        odb.close();
+    }
+
+    public static void salario_menos(ODB odb, int cantidade) {
+        odb = ODBFactory.open(ODB_NAME);
+        IQuery query = odb.criteriaQuery(Player.class, Where.lt("salario", cantidade));
+        Objects<Player> players = odb.getObjects(query);
+        players.size();
+        Player player = null;
+        while (players.hasNext()) {
+            player = players.next();
+            System.out.println(player.getName());
+
+        }
+        odb.close();
+    }
+
+    public static void nomes_por_deporte(ODB odb, String deporte, String nome_novo) {
+        odb = ODBFactory.open(ODB_NAME);
+        IQuery query_aux = odb.criteriaQuery(Sport.class, Where.equal("name", deporte));
+        Objects<Sport> sports = odb.getObjects(query_aux);
+        Sport sport = null;
+        while (sports.hasNext()) {
+            sport = sports.next();
+
+            System.out.println(sport.getName());
+        }
+        IQuery query = odb.criteriaQuery(Player.class, Where.equal("favoriteSport", sport));
+        Objects<Player> players = odb.getObjects(query);
+
+        Player player = null;
+        while (players.hasNext()) {
+            player = players.next();
+            player.setName(nome_novo);
+            odb.store(player);
+
+        }
+        odb.close();
+    }
+
+    public static void nativeQueryXogadoresDeporte(ODB odb, final String deporte) {
+        odb = ODBFactory.open(ODB_NAME);
+        IQuery query_aux = new SimpleNativeQuery() {
+            public boolean match(Player player) {
+                return player.getFavoriteSport().getName().toLowerCase().startsWith(deporte);
+            }
+        };
+        Objects<Player> players = odb.getObjects(query_aux);
+        Player player = null;
+        while (players.hasNext()) {
+            player = players.next();
+            System.out.println(player.getName());
+
+        }
+
+        odb.close();
+    }
+
+    public static void borra_xogadores_por_nome(ODB odb, String nome) {
+            odb = ODBFactory.open(ODB_NAME);
+            IQuery query = odb.criteriaQuery(Player.class, Where.equal("name",nome));
+            Objects<Player> players = odb.getObjects(query);
+            Player player = null;
+            while(players.hasNext()){
+            player=players.next();
+            odb.delete(player);
+           
+            }     
+          odb.close();
+            }
+    public static void listar_xogadores_nome_deporte(ODB odb,String nome,String depor){
+        odb = ODBFactory.open(ODB_NAME);
+        IQuery sport_aux= odb.criteriaQuery(Sport.class, Where.equal("name",depor));
+        Sport sport=(Sport) odb.getObjects(sport_aux).next();
+        System.out.println(sport.getName());
+                       
+        IQuery queryaux =odb.criteriaQuery(Player.class, Where.and().add(Where.equal("favoriteSport.name","Tenis"))
+                           .add(Where.equal("name", nome)));
+        Objects <Player> player=odb.getObjects(queryaux);
+
+        while(player.hasNext()){
+            System.out.println(player.next().getName()+" ");
+            
+        }
+    }
+    public static void actualizar_salario(){
+        IQuery query=odb.criteriaQuery(Team.class, Where.and().add(Where.equal("name","Bordeux")));
+        Team play=(Team) odb.getObjects(query).getFirst();
+        Iterator <Player> player= play.players.iterator();
+       
+        while(player.hasNext()){
+            Player aux=player.next();
+            System.out.println(aux.getName()+" "+aux.getSalario());
+            if(aux.getName().equals("luis")){
+                aux.setSalario(aux.getSalario()+100);
+                 odb.store(aux);
+        
+            }
+        }
+    }
+    }
+    
+    
+
